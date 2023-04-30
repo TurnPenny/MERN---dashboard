d96a36cedd684432b0870b2bb8ce3f7ef6e9abe0
turnPenny <bohdan.miroshnik23@gmail.com>
2023-04-30 13:12:47 +0200

    dark theme mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,19 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { themeSettings } from 'theme';
+
 function App() {
-  return <></>;
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
