486cc64fd154ed19574a122820e5b834c9dd7685
turnPenny <bohdan.miroshnik23@gmail.com>
2023-04-30 11:25:13 +0200

    mongo connect

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
 
+import clientRoutes from './routes/client.js';
+import salesRoutes from './routes/sales.js';
+import managementRoutes from './routes/management.js';
+import generalRoutes from './routes/general.js';
+
 // config
 dotenv.config();
 const app = express();
@@ -23,3 +28,20 @@ app.use('/client', clientRoutes);
 app.use('/general', generalRoutes);
 app.use('/management', managementRoutes);
 app.use('/sales', salesRoutes);
+
+// mongoose
+
+const PORT = process.env.PORT || 3000;
+const URL = process.env.DB_CONNECT;
+
+mongoose
+  .connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
